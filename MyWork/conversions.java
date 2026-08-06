2c51a7d405e6f4433bc91e864525b04d6906d619
Romina <rominajawad132@gmail.com>
2026-08-06 13:53:42 -0400

    Add conversion explanations in conversions.java
    Add comments explaining binary and decimal conversions.

---
+++ MyWork/conversions.java
@@ -0,0 +1,12 @@
+/* Binary/decimal conversions of digits in shape For decimal to binary Take the number, divide it by 2 and then write the remiander which can either be 0 or 1 and write that reminder until the number reaches 0
+
+for example: 7 7/2= 1 1 3/2=1 1 0/2=1
+
+finish when the number reaches 0
+
+for binary to decimal First write the number. Go to the right hand side, start writing base 2 to 0 and increase. Then multiply them and with the numbers you get, add them
+
+For example: 1 1 1 0 0 2^3 2^2 2^1 2^0 16 8 4 2 1
+
+(32)+(16)+(8)+(4)+(2)= 62 
+*/
